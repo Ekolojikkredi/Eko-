04ae1645bc2a61d517f6ef09adbb3a6456898345
Ekolojikkredi <ekolojikkreditubitak@gmail.com>
2024-12-08 20:21:24 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="logo">
+        <img src="assets/images/logo.png" alt="Ekolojik Kredi">
+    </div>
+    <div class="nav">
+        <a href="index.php">Ana Sayfa</a>
+        <a href="register.php">Kayıt Ol</a>
+        <a href="login.php">Giriş Yap</a>
+        <a href="contact.php">Bize Ulaşın</a>
+    </div>
+</header>
